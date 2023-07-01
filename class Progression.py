79334d6a83372666126a1b8ad5c2852ac89903c7
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-07-01 08:40:50 +0530

    Update class Progression.py

--- class Progression.py
+++ class Progression.py
@@ -68,3 +68,20 @@ if __name__ == '__main__':
 
     print('Fibonacci progression with start values 4 and 6:')
     FibonacciProgression(4, 6).print_progression(10)
+
+
+
+
+Default progression:
+0123456789
+Arithmetic progression with increment 5:
+0 5 10 15 20 25 30 35 40 45
+Arithmetic progression with increment 5 and start 2: 2 7 12 17 22 27 32 37 42 47
+Geometric progression with default base:
+1 2 4 8 16 32 64 128 256 512
+Geometric progression with base 3:
+1 3 9 27 81 243 729 2187 6561 19683
+Fibonacci progression with default start values:
+0 1 1 2 3 5 8 13 21 34
+Fibonacci progression with start values 4 and 6:
+4 6 10 16 26 42 68 110 178 288
